50ddf3c85fee0bcd1433e230eabff3949e3e9ce8
Tim Freeman <d.timothy.freeman@gmail.com>
2018-04-16 21:46:53 -0400

    initial commit of basic site

---
+++ src/helpers.js
@@ -0,0 +1,127 @@
+function determineStartOfWeek(today) {
+  let day = today
+  
+  while (day.getDay() !== 1) {
+    day = new Date(day.getTime() - 86400000);
+  }
+  
+  return day
+}
+
+function convertDateToQueryString(dateToConvert) {
+    let month = `0${dateToConvert.getMonth() + 1}`.slice(-2)
+    let date = `0${dateToConvert.getDate()}`.slice(-2)
+    let year = dateToConvert.getFullYear()
+
+    return `${year}${month}${date}`
+  }
+
+function determineEndOfWeek(today) {
+  let day = today
+  
+  while (day.getDay() !== 0) {
+    day = new Date(day.getTime() + 86400000);
+  }
+  
+  return day
+}
+
+function makeTeamObjects(games) {
+  let teamMap = {}
+  games.forEach(function(game) {
+    if (!teamMap[game.homeTeam.Name]) {
+      teamMap[game.homeTeam.Name] = {'games': [], 'abbr': game.homeTeam.Abbreviation}
+    }
+
+    if (!teamMap[game.awayTeam.Name]) {
+      teamMap[game.awayTeam.Name] = {'games': [], 'abbr': game.awayTeam.Abbreviation}
+    }
+
+    teamMap[game.homeTeam.Name]['games'].push(game)
+    teamMap[game.awayTeam.Name]['games'].push(game)
+  })
+
+  teamMap['White Sox'].abbr = 'CHW'
+
+  return teamMap
+}
+
+function makeTeamSeries(teamObjects, scores) {
+  let htmlString = '<div class="row team-series-row">'
+  let columnCheck = 0
+
+  for (let team in teamObjects) {
+    let games = teamObjects[team].games
+    htmlString += `<div class="col-sm individual-team-series">${team}<div><img src="http://a.espncdn.com/combiner/i?img=/i/teamlogos/mlb/500/${teamObjects[team].abbr.toLowerCase()}.png&h=150&w=150"></div>`
+    
+    function homeGameClass(gameScoreObj) {
+      if (gameScoreObj) {
+        if (gameScoreObj.awayScore < gameScoreObj.homeScore) {
+          return 'home-game win'
+        } else {
+          return 'home-game loss'
+        }
+      }
+
+      return 'home-game'
+    }
+
+    function awayGameClass(gameScoreObj) {
+      if (gameScoreObj) {
+        if (gameScoreObj.homeScore < gameScoreObj.awayScore) {
+          return 'away-game win'
+        } else {
+          return 'away-game loss'
+        }
+      }
+
+      return 'away-game'
+    }
+
+    games = games.map(game => {
+      if (game.homeTeam.Name === team) {
+        let str = `<div id=${game.id} class="${homeGameClass(scores[game.id])}">vs ${game.awayTeam.Name}`
+        if (scores[game.id]) {
+          str += ` (${scores[game.id].awayScore || 0} - ${scores[game.id].homeScore || 0})</div>`
+        } else {
+          str += '</div>'
+        }
+        return str
+      } else {
+        let str = `<div id=${game.id} class="${awayGameClass(scores[game.id])}">vs ${game.homeTeam.Name}`
+        if (scores[game.id]) {
+          str += ` (${scores[game.id].awayScore || 0} - ${scores[game.id].homeScore || 0})</div>`
+        } else {
+          str += '</div>'
+        }
+        return str
+      }
+    }).join('')
+
+    htmlString += `${games}</div>`
+    columnCheck++
+    if (columnCheck > 0 && columnCheck % 5 === 0) {
+      htmlString += '</div><br /><br /></be><div class="row team-series-row">'
+    }
+  }
+
+  return `${htmlString}</div>`
+}
+
+function processGames(games, scores) {
+  let teamObjects = makeTeamObjects(games)
+  let gameDivs = makeTeamSeries(teamObjects, scores)
+
+  gameDivs = `<h2>This Week's Series</h2><br /><br />${gameDivs}`
+
+  document.getElementById('games').innerHTML = gameDivs
+}
+
+export const helpers = {
+  determineStartOfWeek,
+  determineEndOfWeek,
+  convertDateToQueryString,
+  makeTeamObjects,
+  makeTeamSeries,
+  processGames
+}
